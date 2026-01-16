b21e86d44c099e3e4f94d8a83e0dfac1d54d497e
Daniel Nygren <dna.swe@gmail.com>
2026-01-16 14:55:54 +0100

    Update android directory
    Use android directory from latest flutter version (3.38.7).

--- android/app/src/main/kotlin/com/example/hegemony_automa_helper/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/hegemony_automa_helper/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.hegemony_automa_helper
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
